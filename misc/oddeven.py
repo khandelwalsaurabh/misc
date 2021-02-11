54ac6eaf62d857f587d72e138c1f5f0aec302b91
khandelwalsaurabh <59793475+khandelwalsaurabh@users.noreply.github.com>
2021-02-11 19:17:35 +0000

    odd even test learning python

---
+++ misc/oddeven.py
@@ -0,0 +1,11 @@
+#to check if a given number is odd or even
+number=input("Enter a number")
+number =int(number)
+print (number%2)
+if number%2==1:
+
+    print("Number is Odd")
+else :
+    print("Number is Even")
+
+
