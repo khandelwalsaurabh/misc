293e06792a6b63037591a83545b18a49296d4177
khandelwalsaurabh <59793475+khandelwalsaurabh@users.noreply.github.com>
2021-02-12 22:05:02 +0000

    remove duplicates from list

---
+++ misc/dupremove.py
@@ -0,0 +1,12 @@
+thislist = [9, 6, 2, 4, 5, 5, 3, 6, 7]
+noduplicates =[]
+for x in thislist:
+    print(x)
+
+    if x not in noduplicates:
+     noduplicates.append(x)
+
+print(noduplicates)
+
+
+
